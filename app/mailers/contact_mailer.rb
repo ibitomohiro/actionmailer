1ac257d7876c2efbe8fcb78deb53fde41feddff6
tomohiro ibi <68018348+ibitomohiro@users.noreply.github.com>
2020-09-13 00:00:34 +0900

    herokuへデプロイ

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,6 +1,6 @@
 class ContactMailer < ApplicationMailer
   def contact_mail(contact)
     @contact = contact
-    mail to: "simonfraser1988@gmail.com", subject:"お問い合わせの確認メール"
+    mail to: "ibitomohiro@gmail.com", subject:"お問い合わせの確認メール"
   end
 end
